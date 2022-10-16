cef072e87f06f66e04135b34c9162a81c9277bc5
QATuuba <drtugbaduran@gmail.com>
2022-10-16 16:15:39 +0100

    Day 18

--- src/test/java/com/cydeo/step_definitions/GoogleStepDefinitions.java
+++ src/test/java/com/cydeo/step_definitions/GoogleStepDefinitions.java
@@ -8,9 +8,6 @@ import org.junit.Assert;
 import org.openqa.selenium.By;
 import org.openqa.selenium.Keys;
 
-import java.awt.dnd.DragGestureEvent;
-import java.security.Key;
-
 public class GoogleStepDefinitions {
 
     @When("user types {string} and clicks enter")
@@ -18,9 +15,10 @@ public class GoogleStepDefinitions {
         googleSearchPage.searchBox.sendKeys(searchKeyword + Keys.ENTER);
     }
 
-    @When("user types {word} and clicks enter")
-    public void user_types_and_clicks_enter2(String searchKeyword) {
-        googleSearchPage.searchBox.sendKeys(searchKeyword + Keys.ENTER);
+
+    @When("user types apple and clicks enter")
+    public void user_types_and_clicks_enter2() {
+        googleSearchPage.searchBox.sendKeys("apple" + Keys.ENTER);
     }
 
     @Then("user sees {string} in the google title")
@@ -28,7 +26,7 @@ public class GoogleStepDefinitions {
         String expectedTitle = string + " - Google Search";
         String actualTitle = Driver.getDriver().getTitle();
 
-        //Junit assertion accepts firts arg as expected, second arg as actual
+        //Junit assertion accepts first arg as expected, second arg as actual
         Assert.assertEquals("Title is not expected!", expectedTitle, actualTitle);
         //Assert.assertTrue(actualTitle.equals(expectedTitle));
     }
@@ -37,13 +35,13 @@ public class GoogleStepDefinitions {
     GoogleSearchPage googleSearchPage = new GoogleSearchPage();
 
 
-    @Then("user sees {word} in the google title")
-    public void user_sees_apple_in_the_google_title(String word) {
-        String expectedTitle = word + " - Google Search";
+    @Then("user sees apple in the google title")
+    public void user_sees_apple_in_the_google_title() {
+        String expectedTitle = "apple - Google Search";
         String actualTitle = Driver.getDriver().getTitle();
 
-        //Junit assertion accepts firts arg as expected, second arg as actual
-        Assert.assertEquals("Title is not expected!", expectedTitle, actualTitle);
+        //Junit assertion accepts first arg as expected, second arg as actual
+        Assert.assertEquals("Title is not as expected!", expectedTitle, actualTitle);
         //Assert.assertTrue(actualTitle.equals(expectedTitle));
     }
 
